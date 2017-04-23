a960e8a357f07d345604380c25eb5fae7436e88b
Noahkiq <noahkiqgaming@gmail.com>
2017-04-22 23:52:51 -0400

    basic twowbot code

--- TWOWBot/Program.cs
+++ TWOWBot/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Discord;
 using Discord.Commands;
 using System.IO;
+using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -12,32 +14,54 @@ class Program
 	{
 		_client = new DiscordClient();
 
+		_client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity} - {DateTime.UtcNow.Hour}:{DateTime.UtcNow.Minute}:{DateTime.UtcNow.Second}] {e.Source}: {e.Message}");
+
 		_client.UsingCommands(x => {
 			x.PrefixChar = '+';
+			x.AllowMentionPrefix = true;
 			x.HelpMode = HelpMode.Public;
 		});
 
-		char p = '+';
+		// char p = '+';
 
-		_client.GetService<CommandService>().CreateGroup("do", cgb =>
-			{
-				cgb.CreateCommand("help")
-					.Alias(new string[] { "pls-assist-me" })
-					.Description("Tells you to not use this.")
-					.Do(async e =>
-						{
-							await e.Channel.SendMessage($"The help command is actually `{p}help mini`. Yeah, I know, it's confusing.");
-						});
+		_client.GetService<CommandService>().CreateCommand("botok") //create command
+		       .Alias("ping", "status") // add some aliases
+               .Description("Checks if the bot is online.") //add description, it will be shown when +help is used
+               .Do(async e => {
+            await e.Channel.SendMessage($"bot is online \ud83d\udc4c");
+               });
 
-				cgb.CreateCommand("create")
-					.Alias(new string[] { "new", "make" })
-					.Description("Creates a Mini TWOW.")
-					.Parameter("GreetedPerson", ParameterType.Required)
-					.Do(async e =>
-						{
-							await e.Channel.SendMessage($"this isn't even started yet scrub");
-						});
-			});
+		_client.GetService<CommandService>().CreateCommand("create")
+			   .Do(async e =>
+			   {
+			   });
+			
+
+		_client.GetService<CommandService>().CreateGroup("test", cgb =>
+		{
+	    	cgb.CreateCommand("data-save")
+				.Description("Multi-server data test")
+				.Parameter("data", ParameterType.Required)
+				.Do(async e =>
+				{
+				    // e.GetArg("GreetedPerson");
+				    await e.Channel.SendMessage($"data saved");
+				});
+
+			cgb.CreateCommand("data-load")
+				.Description("Multi-server data test")
+				//.Parameter("data", ParameterType.Required)
+				.Do(async e =>
+				{
+					string data = "unfinished";
+				    await e.Channel.SendMessage($"saved data: {data}");
+				});
+		});
+
+		_client.Ready += (s, e) =>
+		{
+			Console.WriteLine($"[{DateTime.UtcNow.Hour}:{DateTime.UtcNow.Minute}:{DateTime.UtcNow.Second}] Connected as {_client.CurrentUser.Name}#{_client.CurrentUser.Discriminator}");
+		};
 
 		_client.ExecuteAndWait(async () => {
 			string token = File.ReadAllLines("token.txt")[0];
